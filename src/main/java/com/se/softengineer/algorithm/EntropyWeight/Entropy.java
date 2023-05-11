713deb8c35c8dc2f48d02a227aad86ec247809e8
南希诺 <nanxi27@bjfu.edu.cn>
2023-05-11 17:48:43 +0800

    确保算法没有错误，各项指标的权重已经算好了。 但是，但是啊，但是 PDF 中，人家的 15 个指标，是随便选的啊（我要闹了.jpg） 晚上回来看看指标体系，看看返回什么，来决定我的 Map 里都放些什么，反正排序算法写好了

--- src/main/java/com/se/softengineer/algorithm/EntropyWeight/Entropy.java
+++ src/main/java/com/se/softengineer/algorithm/EntropyWeight/Entropy.java
@@ -7,9 +7,8 @@ import org.apache.catalina.mbeans.SparseUserDatabaseMBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @Author 南希诺
@@ -28,30 +27,37 @@ public class Entropy {
      */
     List<Double> stdY;
     /**
+     * 每个指标自己所有数据标准化后的和
+     */
+    List<Double> sumYij;
+    /**
      * 存放 P 值的数组
      */
     List<Double> pY;
     /**
      * 存放每一个指标信息熵的数组
      */
-    List<Double> infoEntropyList;
+    List<Double> EList;
     /**
      * 每一个指标权重的数组
      */
     List<Double> WList;
+    /**
+     * 评分数组
+     */
     List<Double> SList;
     /**
-     * stdYij 的和
+     * 每个指标的评分 Map
      */
-    double sumYij;
-    /**
-     * 熵权数组的总和
-     */
-    double sumEj;
+    Map<String, Double> resultMap;
     /**
      * 权重数组的总和
      */
     double sumWj;
+    /**
+     * 信息熵和
+     */
+    double sumEj;
     /**
      * todo: change 指标的个数
      */
@@ -67,18 +73,20 @@ public class Entropy {
 //        System.out.println(stdY);
         // 2.求各指标的信息熵
         infoEntropy();
-//         System.out.println(infoEntropyList);
+//         System.out.println(EList);
         // 3.通过信息熵计算各指标的权重
         Weight();
 //        System.out.println(WList);
         // 4.评分（可选）
-        score();
-        System.out.println(SList);
+        //score();
+//        System.out.println(SList);
+        // 5.验证阶段：取前 15 最大的，和 PDF 比较
+        sortMap();
 
     }
 
     /**
-     * 数据标准化
+     * 数据标准化，该步正确
      */
     public void standardize() {
         // 初始化标准化数组，数组的大小就是指标的个数
@@ -97,15 +105,53 @@ public class Entropy {
                 maxXi = Math.max(value, maxXi);
                 minXi = Math.min(value, minXi);
             }
-
+            /*
+              到这一步，一个指标内的最大值和最小值就算出来了
+              已验证
+             */
             // 最小-最大规范化
             for (int i = 0; i < idxChild; i++) {
                 double value = dataList.get(times * idxChild + i);
                 double Yij = (value - minXi) / (maxXi - minXi);
                 stdY.add(Yij);
-                sumYij += Yij;
             }
         }
+        // 计算 sumYij
+        calSumYij();
+    }
+
+    /**
+     * 计算 sumYij
+     */
+    public void calSumYij() {
+        sumYij = new ArrayList<>(indexNumber);
+        double sum = 0.0;
+        int j = 0;
+        for (int i = 0; i < stdY.size(); i++) {
+            // 下一个轮次
+            if (i / idxChild != j) {
+                sumYij.add(sum);
+                sum = 0.0;
+                j = i / idxChild;
+                i -= 1;
+            } else {
+                sum += stdY.get(i);
+            }
+        }
+        sumYij.add(sum);
+
+//        // test ok
+//        double s = 0.0;
+//        for (int i = 0; i < 22; i++) {
+//            s += stdY.get(i);
+//        }
+//        System.out.println(s);
+//        s = 0.0;
+//        for (int i = 22; i < 44; i++) {
+//            s += stdY.get(i);
+//        }
+//        System.out.println(s);
+
     }
 
     /**
@@ -114,28 +160,34 @@ public class Entropy {
     public void infoEntropy() {
         // 初始化 pY
         pY = new ArrayList<>(stdY.size());
-        for (Double aDouble : stdY) {
-            // todo:这里做了 10 倍放大处理，不然 pY 太小了
-            pY.add(10 * aDouble / sumYij);
+        for (int i = 0; i < stdY.size(); i++) {
+            double sum = sumYij.get(i / idxChild);
+            pY.add(stdY.get(i) / sum);
         }
         // 初始化 EList,有多少个指标就有多少组信息熵
         // 所以该数组的大小是指标的个数
-        infoEntropyList = new ArrayList<>(indexNumber);
-        double sumPij;
-        // 有几个指标就需要循环几次
-        for (int times = 0; times < indexNumber; times++) {
-            sumPij = 0.0;
-            // 内循环一个指标的数据个数，计算总 P 值
-            for (int i = 0; i < idxChild; i++) {
-                // 起始值：times * idxChild，逐个增加 i
-                double value = pY.get(times * idxChild + i);
-                sumPij += value == 0 ? 0 : value * Math.log(value);
+        EList = new ArrayList<>(indexNumber);
+        double sum = 0.0;
+        int j = 0;
+        for (int i = 0; i < pY.size(); i++) {
+            // 下一个轮次
+            if (i / idxChild != j) {
+                double Ej = (-1) * (1 / Math.log(idxChild)) * sum;
+                EList.add(Ej);
+                sumEj += Ej;
+                sum = 0.0;
+                j = i / idxChild;
+                i -= 1;
+            } else {
+                double value = pY.get(i);
+                sum += (value == 0 ? 0 : value * Math.log(value));
             }
-            double Ej = (-1) * (1 / Math.log(idxChild)) * sumPij;
-            infoEntropyList.add(Ej);
-            sumEj += Ej;
         }
+        double Ej = (-1) * (1 / Math.log(idxChild)) * sum;
+        EList.add(Ej);
+        sumEj += Ej;
     }
+
 
     /**
      * 求权重
@@ -143,12 +195,31 @@ public class Entropy {
     public void Weight() {
         // 初始化数组
         WList = new ArrayList<>(indexNumber);
-        for (Double aDouble : infoEntropyList) {
+        resultMap = new HashMap<>();
+        int i = 1;
+        for (Double aDouble : EList) {
             // 这里 k 指的是指标个数
-            // todo:为显示方便，这里做了 * 10 放大处理
-            double Wi = 10 * (1 - aDouble) / (indexNumber - sumEj);
+            double Wi = (1 - aDouble) / (indexNumber - sumEj);
             WList.add(Wi);
+            //resultMap.put("X" + i, Wi);
+            //i++;
             sumWj += Wi;
+        }
+
+        double sum = 0.0;
+        for (int j = 0; j < WList.size(); j++) {
+            sum += WList.get(j);
+            if (j == 6) {
+                resultMap.put("第一类指标", sum);
+                sum = 0.0;
+            } else if (j == 16) {
+                resultMap.put("第二类指标", sum);
+                sum = 0.0;
+            } else if (j == 22) {
+                resultMap.put("第三类指标", sum);
+                sum = 0.0;
+            }
+
         }
     }
 
@@ -157,12 +228,14 @@ public class Entropy {
      */
     public void score() {
         SList = new ArrayList<>(indexNumber);
+        resultMap = new HashMap<>();
         double score = 0.0;
         int j = 0;
         for (int i = 0; i < pY.size(); i++) {
+            // 是下一个指标的数据了。
             if (i / idxChild != j) {
-                //todo:为方便观察，这里对 score 做了 100 倍放大处理
-                SList.add(100 * score);
+                SList.add(score);
+                resultMap.put("X" + (j + 1), score);
                 score = 0.0;
                 j = i / idxChild;
                 i -= 1;
@@ -171,8 +244,30 @@ public class Entropy {
             }
         }
         // 最后再把这个指标加进去
-        SList.add(100 * score);
+        SList.add(score);
+        resultMap.put("X" + (j + 1), score);
     }
+
+    /**
+     * 根据评分对指标进行排序
+     */
+    public void sortMap() {
+        // 将 Map 转换成 List 集合，每个元素是一个 Map.Entry 对象
+        List<Map.Entry<String, Double>> entryList = new ArrayList<>(resultMap.entrySet());
+
+        // 使用 Stream API 对 List 集合进行排序，排序方式为按照 Entry 对象的值从高到低排序
+        entryList = entryList.stream()
+                .sorted((e1, e2) -> Double.compare(e2.getValue(), e1.getValue()))
+                .collect(Collectors.toList());
+
+        // 将排好序的 Entry 对象重新存放到一个 LinkedHashMap 中，以保证排序后的顺序不被打乱
+        LinkedHashMap<String, Double> sortedMap = new LinkedHashMap<>();
+        entryList.forEach(entry -> sortedMap.put(entry.getKey(), entry.getValue()));
+
+        // 打印排序后的Map
+        System.out.println(sortedMap);
+    }
+
 
 
 }
