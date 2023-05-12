0d34a9254fb6baf549bd92ef23edf1abd866cb6b
yuzuwxy <w1149639121@163.com>
2023-05-12 22:55:19 +0800

    感动死我了 1. Mapper.xml的namespace上一版写错了，已修改 2. pom.xml里build添加了<filter>，但是应该也不需要 3. application.properties文件根据本地情况做了更改 在IndexSymController的loadData方法可以正确返回结果，数据库可以正确读取 接下来尝试加载data表里的数据

--- src/main/java/com/se/softengineer/controller/IndexSymController.java
+++ src/main/java/com/se/softengineer/controller/IndexSymController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * by wxy
  * 试试springboot
@@ -26,17 +28,20 @@ public class IndexSymController {
     /*private List<Data> data;
     // 想试一下这种写法能不能载入数据*/
 
+    /**by wxy
+     * Just for test
+     **/
     @GetMapping("/test")
     private String test() {
         return "Hello World!";
     }
 
     @GetMapping("/loadData")
-    private boolean load_data() {
+    private List<Node> load_data() {
         indexSym.setNodeList(indexSymService.getIndex());
         for(int i = 0; i < indexSym.getNodeList().size(); i ++)
             System.out.println(indexSym.getNodeList().get(i));
-        return true;
+        return indexSym.getNodeList();
     }
 
     @GetMapping("/usePCA")
