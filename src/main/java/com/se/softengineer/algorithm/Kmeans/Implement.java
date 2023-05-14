9d0430a068bd41055b0db68b08a57aeb7d8e3fcc
xly <3090057208@qq.com>
2023-05-14 14:31:45 +0800

    改了一点点

--- src/main/java/com/se/softengineer/algorithm/Kmeans/Implement.java
+++ src/main/java/com/se/softengineer/algorithm/Kmeans/Implement.java
@@ -1,6 +1,8 @@
 package com.se.softengineer.algorithm.Kmeans;
 
 import com.se.softengineer.algorithm.indexsym.Data;
+import com.se.softengineer.algorithm.indexsym.IndexSym;
+import com.se.softengineer.algorithm.indexsym.Node;
 import com.se.softengineer.algorithm.trydatabase.TestMySQL;
 
 import java.util.List;
@@ -15,6 +17,7 @@ public class Implement {
         ElbowMethod elbowMethod = new ElbowMethod();
         TestMySQL testMySQL = new TestMySQL();
         List<List<Double>> dataSet = testMySQL.queryData("data");
+//        IndexSym indexSym = new IndexSym(testMySQL.querySym("IndexSym"));
         Data data = new Data(dataSet);
         int maxk = data.getSample_num()/2;
         System.out.println(data.getSample_num());
@@ -23,14 +26,17 @@ public class Implement {
         Kmeans kRun =new Kmeans(k, data);
         Set<Cluster> clusterSet = kRun.run();
         System.out.println("单次迭代运行次数："+kRun.getIterRunTimes());
+        int i=1;
         for (Cluster cluster : clusterSet) {
+            Node fatherNode = new Node(i,"father"+i,1,1.0,0);
             System.out.println(cluster);
+//            Point center = cluster.getCenter();
+            List<Point> points = cluster.getMembers();
         }
     }
 
     public static void main(String[] args) throws Exception {
         Implement implement = new Implement();
         implement.run();
-
     }
 }
